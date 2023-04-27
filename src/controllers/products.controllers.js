caa3d37cc0b35685016d3f87b0251903f31756cb
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-04-27 17:50:15 -0300

    req 04 - validacoes de name

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -14,8 +14,13 @@ const getProducts = async (req, res) => {
 
 const create = async (req, res) => {
   const { name } = req.body;
+  if (!name) return res.status(400).json({ message: '"name" is required' });
+  if (name.length < 5) {
+    return res.status(422).json(
+      { message: '"name" length must be at least 5 characters long' },
+    );
+  }
   const { message } = await productsServices.createProduct(name);
-  console.log(message);
   return res.status(201).json(message);
 };
 module.exports = {
